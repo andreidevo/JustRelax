7faaa2efca130170a136067b6c81cd483688fa7c
Андрей Морозов <ezforlive2@gmail.com>
2019-06-06 01:51:10 +0300

    : try new animation. Shadow screen and close popup by click

--- app/src/main/java/justrelax/justrelax/VideoAdapterKot.kt
+++ app/src/main/java/justrelax/justrelax/VideoAdapterKot.kt
@@ -1,17 +1,25 @@
 package justrelax.justrelax
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.support.constraint.ConstraintLayout
 import android.support.v7.view.menu.MenuView
 import android.support.v7.widget.RecyclerView
+import android.transition.Fade
 import android.transition.Slide
 import android.transition.TransitionManager
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.PopupWindow
@@ -72,47 +80,49 @@ class VideoAdapterKot(private val mExampleList: ArrayList<VideoClass>, layout: C
 
         var inflater: LayoutInflater  = holder.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         var view = inflater.inflate(R.layout.popupfragment, null)
-        val popupWindow = PopupWindow(
-        view, // Custom view to show in popup window
-        LinearLayout.LayoutParams.WRAP_CONTENT, // Width of popup window
-        LinearLayout.LayoutParams.WRAP_CONTENT // Window height
-        )
 
-        // Set an elevation for the popup window
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        popupWindow.elevation = 10.0F
+
+        val popupWindow = PopupWindow(view, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+          //  popupWindow.animationStyle = R.animator.alpha - not work
+
+        /*   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.elevation = 10.0F
         }
+            //
 
-
-        // If API level 23 or higher then execute the code
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-        // Create a new slide animation for popup window enter transition
         val slideIn = Slide()
         slideIn.slideEdge = Gravity.TOP
         popupWindow.enterTransition = slideIn
 
-        // Slide animation for popup window exit transition
         val slideOut = Slide()
         slideOut.slideEdge = Gravity.RIGHT
         popupWindow.exitTransition = slideOut
 
         }
+           //
+            */
+        //popupWindow.setOnDismissListener {
+        //Toast.makeText(holder.context,"Popup closed", Toast.LENGTH_SHORT).show()
+        //}
 
 
-        // Set a dismiss listener for popup window
-        popupWindow.setOnDismissListener {
-        Toast.makeText(holder.context,"Popup closed", Toast.LENGTH_SHORT).show()
-        }
+            //Тень.
+            var shadow: ConstraintLayout = view.findViewById(R.id.contento)
+            shadow.setBackgroundColor(Color.parseColor("#DD000000"))
+            //листенер на тень.
+            shadow.setOnClickListener {
+                popupWindow.dismiss()
+            }
+
+           // val animation :Animation = AlphaAnimation(0.0f,1.0f)
+           //   animation.duration = 100
+           // animation.startOffset = 20
+           // animation.repeatCount = Animation.INFINITE
+           // animation.repeatMode = Animation.REVERSE
 
         TransitionManager.beginDelayedTransition(mLay)
-        popupWindow.showAtLocation(
-            mLay, // Location to display popup window
-        Gravity.CENTER, // Exact position of layout to display popup
-        0, // X offset
-        0 // Y offset
-        )
-
-
+        popupWindow.showAtLocation(mLay, Gravity.CENTER, 0, 0)
         }
     }
 
