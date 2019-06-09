ed5b1e23d0f56f3804f73c4e98d5fc1db98b0b98
Андрей Морозов <ezforlive2@gmail.com>
2019-06-09 19:26:54 +0300

    Comments

--- app/src/main/java/justrelax/justrelax/VideoAdapterKot.kt
+++ app/src/main/java/justrelax/justrelax/VideoAdapterKot.kt
@@ -31,14 +31,14 @@ import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.*
 import justrelax.justrelax.R.id.root
 
 import java.util.ArrayList
-
-
-
-//Download, music, animation
 
 class VideoAdapterKot(private val mExampleList: ArrayList<VideoClass>, layout: ConstraintLayout) :
     RecyclerView.Adapter<VideoAdapterKot.ExampleViewHolder>() {
@@ -62,52 +62,62 @@ class VideoAdapterKot(private val mExampleList: ArrayList<VideoClass>, layout: C
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.fragment_video, parent, false)
-
         return ExampleViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
         val currentItem = mExampleList[position]
         var params : ViewGroup.LayoutParams = holder.CardView.layoutParams
+
+        /*Растяжение карточек на какую-то высоту*/
         params.height = (holder.CardView.resources.displayMetrics.density * currentItem.height).toInt() // типа 20dp
         holder.CardView.layoutParams = params
-
-        //var animation:Animation =  AnimationUtils.loadAnimation(holder.context, R.anim.anim1)
-        //holder.CardView.animation = animation
+        /*Нажатие на карточку*/
         holder.image.setOnClickListener {
-        var inflater: LayoutInflater  = holder.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        var view = inflater.inflate(R.layout.popupfragment, null)
-        val popupWindow = PopupWindow(view, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
-        //Тень.
-        var shadow: ConstraintLayout = view.findViewById(R.id.contento)
-      var headphones: ImageView = view.findViewById(R.id.pic1)
-//      var download: ImageView = view.findViewById(R.id.pic2)
 
-        var VideoDescription : TextView = view.findViewById(R.id.Description)
-        VideoDescription.setText(currentItem.description)
-        headphones.setOnClickListener{
+            /*Взятие контекста*/
+            var inflater: LayoutInflater  = holder.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            /*Создание вью*/
+            var view = inflater.inflate(R.layout.popupfragment, null)
+            /*Определение попапа*/
+            val popupWindow = PopupWindow(view, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            /*действия при нажатии на тень - закрытие попапа*/
+            var shadow: ConstraintLayout = view.findViewById(R.id.contento)
+            shadow.setOnClickListener {
+                popupWindow.dismiss()
+            }
+            /*нажатие на наушник - открытие нижнего меню - музыки*/
+            var headphones: ImageView = view.findViewById(R.id.pic1)
+            /*Нажатие на иконку загрузки*/
+            //var download: ImageView = view.findViewById(R.id.pic2)
+            /*Описание видео*/
+            var VideoDescription : TextView = view.findViewById(R.id.Description)
+            VideoDescription.setText(currentItem.description)
 
-            var some : android.support.design.widget.CoordinatorLayout = view.findViewById(R.id.someCoordianal)
-            var llBottomSheet: android.support.v7.widget.LinearLayoutCompat?? =  view.findViewById(R.id.bottom2) as? android.support.v7.widget.LinearLayoutCompat
-            some.bringToFront()
-            var bottom  = BottomSheetBehavior.from(llBottomSheet)
-            bottom.setState(BottomSheetBehavior.STATE_EXPANDED)
-
-
+            /*нажатие на иконку нашуников*/
+            headphones.setOnClickListener{
+                /*определение лэйаута*/
+                var some : android.support.design.widget.CoordinatorLayout = view.findViewById(R.id.someCoordianal)
+                /*определение меню*/
+                var llBottomSheet: android.support.v7.widget.LinearLayoutCompat?? =  view.findViewById(R.id.bottom2) as? android.support.v7.widget.LinearLayoutCompat
+                /*меню выдвигается*/
+                var bottom  = BottomSheetBehavior.from(llBottomSheet)
+                bottom.setState(BottomSheetBehavior.STATE_EXPANDED)
         }
-            // загрузка видео
-        //download.setOnClickListener() // выбор музыки
-        //shadow.setBackgroundColor(Color.parseColor("#DD000000"))
-        //листенер на тень.
-        shadow.setOnClickListener {
-            popupWindow.dismiss()
-        }
 
-
-        /*Video PLayer */
+//        var  webView  = view.findViewById(R.id.myweb) as WebView
+//            webView.setWebViewClient( WebViewClient());
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+//        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+//        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+//        webView.setWebChromeClient( WebChromeClient());
+//        webView.loadUrl("https://www.dropbox.com/s/gys5q7yc868ko59/52586642_324829134816014_3179773492790820864_n.mp4?dl=1")
+//        /*Video PLayer */
         val VideoClasser = VideoClasser(view, "asdasd", holder.context)
         VideoClasser.CreateVideo()
         TransitionManager.beginDelayedTransition(mLay)
+        /*Создание попапа по центру лэйаута*/
         popupWindow.showAtLocation(mLay, Gravity.CENTER, 0, 0)
 
         }
@@ -119,7 +129,7 @@ class VideoAdapterKot(private val mExampleList: ArrayList<VideoClass>, layout: C
 }
 
 class VideoClasser(view: View, scr : String, base: Context) : ContextWrapper(base){//mb error
-
+    /*пока что не нужный конструктор*/
     private var scres : String = ""
     init {
          scres = scr
@@ -140,13 +150,14 @@ class VideoClasser(view: View, scr : String, base: Context) : ContextWrapper(bas
             PauseButton.visibility = View.INVISIBLE
         }
     }
+     /*Основная часть*/
      fun CreateVideo()  {
         val Src = scres
         Log.d("myVideo", Src)
-        val scr : Uri = Uri.parse("https://www.dropbox.com/s/wk4iitsmlj9ph3f/smalltest.mp4?dl=1")
+        val scr : Uri = Uri.parse("https://www.dropbox.com/s/gys5q7yc868ko59/52586642_324829134816014_3179773492790820864_n.mp4?dl=1")
         Video.setOnClickListener(::StopPlay)
         PauseButton.setOnClickListener(::StopPlay)
-        Video.setVideoPath("https://www.dropbox.com/s/gys5q7yc868ko59/52586642_324829134816014_3179773492790820864_n.mp4?dl=1")
+        Video.setVideoURI(scr)
         Video.start()
     }
 }
